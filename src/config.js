fa8aa6e84ab7035c84a1959e981cd28072c724e4
nick_richardson <nick.richardson@rezonence.com>
2019-07-02 11:35:31 +0100

    Create login page

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "eu-west-2",
+        BUCKET: "golden-slipper"
+    },
+    apiGateway: {
+        REGION: "us-east-1",
+        URL: "https://qds38iwumg.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_xRDyDgmXr",
+        APP_CLIENT_ID: "5ts43uo9ggu61nt2tu4psrcoib",
+        IDENTITY_POOL_ID: "us-east-1:bc5d0a93-1242-4286-ab30-3218621dc0ca"
+    }
+};
