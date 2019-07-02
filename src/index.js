25c1ae227d092f6900be9910a0ab74b476390f1c
nick_richardson <nick.richardson@rezonence.com>
2019-07-02 12:13:34 +0100

    Create signup page

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from "react-router-dom";
-import Amplify from "aws-amplify";
+import Amplify, {Analytics} from "aws-amplify";
 import config from "./config";
 
 Amplify.configure({
@@ -30,6 +30,8 @@ Amplify.configure({
         ]
     }
 });
+
+Analytics.disable();
 
 ReactDOM.render(
     <Router>
